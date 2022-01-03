8072f3bdb9ec3a2e048d913a16a3aba83c9dfebf
sergioliveres <soliveres@thehotelsnetwork.com>
2022-01-03 19:58:57 +0100

    feat: Mars Rover has a starting point and a position

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 
 namespace MarsRover;
 
+use MarsRover\ValueObject\Position;
+
 final class MarsRover
 {
+    public function __construct(private Position $position, private string $direction)
+    {
+    }
 
+    public function latitude(): int
+    {
+        return $this->position->latitude();
+    }
+
+    public function longitude(): int
+    {
+        return $this->position->longitude();
+    }
+
+    public function direction()
+    {
+        return $this->direction;
+    }
 }
--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -1,8 +1,17 @@
 <?php
 
+use MarsRover\MarsRover;
+use MarsRover\ValueObject\Position;
 use PHPUnit\Framework\TestCase;
 
 final class MarsRoverTest extends TestCase
 {
+    public function test_mars_rover_is_instantiated_with_given_position_and_direction()
+    {
+        $rover = new MarsRover(new Position(1, 2), 'W');
 
+        $this->assertEquals(1, $rover->latitude());
+        $this->assertEquals(2, $rover->longitude());
+        $this->assertEquals('W', $rover->direction());
+    }
 }
