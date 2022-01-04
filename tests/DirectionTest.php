d86cc0a5949260e1dbd24ea7f7a1c3b99abdfd74
sergioliveres <soliveres@thehotelsnetwork.com>
2022-01-04 14:05:39 +0100

    test: cover edge case

--- tests/DirectionTest.php
+++ tests/DirectionTest.php
@@ -6,6 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class DirectionTest extends TestCase
 {
+    public function test_direction_is_north_when_by_default()
+    {
+        $direction = new Direction();
+
+        $this->assertEquals(Direction::NORTH, $direction->value());
+    }
+
     public function test_direction_should_throw_and_exception_when_invalid_direction_given()
     {
         $this->expectException(InvalidDirectionException::class);
